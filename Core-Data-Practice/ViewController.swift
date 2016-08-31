21009510b519c5db51dec24a303105347c3aecd3
Kevin Hou <kevin.ch.hou@gmail.com>
2016-08-31 18:01:00 -0400

    Setup table view
    No functionality yet

--- Core-Data-Practice/ViewController.swift
+++ Core-Data-Practice/ViewController.swift
@@ -8,11 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet var tableView: UITableView! // Main table view
+    var names = [String]() // Initialize empty array with type String
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        title = "\"The List\"" // Set title
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func addName(sender: AnyObject) {
+        
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let rowIndex = indexPath.row // Store row index
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell")
+        
+        cell!.textLabel?.text = names[rowIndex] // Set text of cell
+        
+        return cell!
+    }
 
 }
 
